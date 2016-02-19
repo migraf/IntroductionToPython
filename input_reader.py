9f62b4db6a8ed33c5177462b95a63aaff93cbc12
migraf <michael.m.graf@gmx.de>
2016-02-19 20:27:48 +0100

    seperate list for languages

--- input_reader.py
+++ input_reader.py
@@ -1,9 +1,11 @@
 language_and_words = {}
+language_list = []
 
 def read_asjp_file(filename):
     return open(str(filename), "r")
 #Going through the input file and finding the entries for the different languages
-#Then taking the next 40 words after a found language and putting them into the dictioniary with the language
+#Then taking the next 40 words after a found language and putting them into the dictionary with the language
+# as well as adding the languages to a list
 
 def split_file(textfile):
     lines = textfile.readlines()
@@ -14,6 +16,7 @@ def split_file(textfile):
 
         #finding a language entry
         if not line[0].isalnum():
+            language_list.append(line[0].split("{")[0])
             #adding the next 40 words(excluding the already calculated score
             word_list = lines[k+1:k+41]
             final_wordlist = []
