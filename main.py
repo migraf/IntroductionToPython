c8ecf8a2899bd9b96fe2147c8cba0a04dda2528f
Julian Petruck <julian.petruck@student.uni-tuebingen.de>
2016-03-11 16:49:09 +0100

    comments

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 __author__ = "Julian Petruck"
 
+# Necessary input.
 from Tkinter import *
 from gui import *
 from input_reader import *
 
-
+# Creates the used language list.
 language_dict = split_file("listss16.txt")
 languages = dict_keys_as_list(language_dict)
 
+# Creates the GUI frame and the GUI class and sets the title.
 frame = Tk()
 gui = GUI(frame)
 frame.title("Distance Methods on ASJP-Corpus")
 
+# Sets the language list of the GUI.
 gui.set_languages(languages)
 
+# Starts the mainloop of the GUI.
 frame.mainloop()
 
